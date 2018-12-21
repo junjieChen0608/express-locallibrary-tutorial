3752e15f3861fa85989771d44923623b1ced2929
junjiechen <junjie.chen@jingchi.ai>
2018-12-21 18:00:32 +0800

    simple you're cool url

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* your are so cool URL */
+router.get('/cool', (req, res, next) => {
+  res.send("You're so cool");
+});
 module.exports = router;
