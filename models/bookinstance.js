018917d3556fbc7a68056ccda22620e3b37cc82f
junjiechen <junjie.chen@jingchi.ai>
2019-01-02 17:47:53 +0800

    implemented book instance detail rendering

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -5,8 +5,8 @@ var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema(
   {
-    //reference to the associated book
-    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true }, 
+    // reference to the associated book
+    book: {type: Schema.Types.ObjectId, ref: 'Book', required: true},
     imprint: {type: String, required: true},
     status: {type: String, required: true,
              enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
