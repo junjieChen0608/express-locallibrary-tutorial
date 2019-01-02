9aef0e5e81c2bf606e4c9bef953a50e7ecbf9e15
junjiechen <junjie.chen@jingchi.ai>
2019-01-02 16:43:36 +0800

    implemented author detail rendering

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,6 @@
 var Author = require('../models/author');
+var Book = require('../models/book');
+var async = require('async');
 
 // Display list of all Authors.
 exports.author_list = function(req, res) {
@@ -15,8 +17,30 @@ exports.author_list = function(req, res) {
 };
 
 // Display detail page for a specific Author.
-exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+exports.author_detail = function(req, res, next) {
+    async.parallel({
+      author: function(callback) {
+        Author.findById(req.params.id)
+              .exec(callback);
+      },
+
+      author_books: function(callback) {
+        Book.find({'author': req.params.id}, 'title summary')
+            .exec(callback);
+      },
+    }, function(err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        var err = new Error('Author not found');
+        err.status = 404;
+        next(err);
+      }
+      res.render('author_detail',
+                 {author: results.author,
+                  author_books: results.author_books});
+    }); 
 };
 
 // Display Author create form on GET.
